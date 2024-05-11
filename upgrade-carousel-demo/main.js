360d297b85c869dffcd4af444311f466e267ba61
xue <2723709118@qq.com>
2024-05-11 21:39:19 +0800

    无缝轮播

---
+++ upgrade-carousel-demo/main.js
@@ -0,0 +1,24 @@
+window.$ = jQuery
+setTimeout(function () {
+    $('.images>img:nth-child(1)').css({
+        transform: 'translateX(-100%)'
+    })
+    $('.images>img:nth-child(2)').css({
+        transform: 'translateX(-100%)'
+    })
+    $('.images>img:nth-child(1)').on('transitionend', function(e){
+        $(e.currentTarget).addClass('right').css({transform: 'none'})
+    })
+}, 3000)
+
+setTimeout(function () {
+    $('.images>img:nth-child(2)').css({
+        transform: 'translateX(-200%)'
+    })
+    $('.images>img:nth-child(3)').css({
+        transform: 'translateX(-100%)'
+    })
+}, 6000)
+
+
+
