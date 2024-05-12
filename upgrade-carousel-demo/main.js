ae820b054f0a5bd2377ce94f5b51922dfa2d795a
xue <2723709118@qq.com>
2024-05-12 10:31:17 +0800

    优化代码：状态机

--- upgrade-carousel-demo/main.js
+++ upgrade-carousel-demo/main.js
@@ -1,16 +1,46 @@
 window.$ = jQuery
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n = 1
+let n
+初始化()
 setInterval(() => {
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
         .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+            makeEnter($(e.currentTarget))
         })
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n + 1))
     n += 1
 }, 3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 下面都是封装函数
+
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
 
 function x(n) {
     if (n > 3) {
@@ -22,6 +52,24 @@ function x(n) {
     return n
 }
 
+function 初始化() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node) {
+    return $node.removeClass('current enter').addClass('leave')
+    //$node.removeClass会return $node，function的返回值是undefined，没有one属性
+}
+
+function makeEnter($node) {
+    return $node.removeClass('current leave').addClass('enter')
+}
 
 
 
