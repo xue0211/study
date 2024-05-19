9a8c6c4311d842dd39d4f1cc0676d51e09e05681
xue <2723709118@qq.com>
2024-05-19 08:53:05 +0800

    修正BUG

--- upgrade-carousel-demo/main.js
+++ upgrade-carousel-demo/main.js
@@ -1,14 +1,30 @@
 window.$ = jQuery
 let n
 初始化()
-setInterval(() => {
+let timer = setInterval(() => {
     makeLeave(getImage(n))
         .one('transitionend', (e) => {
             makeEnter($(e.currentTarget))
         })
     makeCurrent(getImage(n + 1))
     n += 1
-}, 3000)
+}, 2000)
+
+document.addEventListener('visibilitychange', function (e) {
+    // console.log(document.hidden)
+    if (document.hidden) {
+        window.clearInterval(timer)
+    } else {
+       timer = setInterval(() => {
+            makeLeave(getImage(n))
+                .one('transitionend', (e) => {
+                    makeEnter($(e.currentTarget))
+                })
+            makeCurrent(getImage(n + 1))
+            n += 1
+        }, 2000)
+    }
+})
 
 
 
