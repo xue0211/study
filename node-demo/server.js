2f94574b336a5e9cc9d082e2641018442ecc8cdc
xue <2723709118@qq.com>
2024-05-21 10:59:22 +0800

    修改server.js

--- node-demo/server.js
+++ node-demo/server.js
@@ -3,55 +3,29 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
-  if(path === '/style'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{background-color: #ddd;}h1{color:green;}`)
-    response.end()
-  } else if(path === '/script'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-    response.write(`alert("这是JS执行的")`)
-    response.end()
-  } else if(path === '/index'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`<!DOCTYPE>\n<html>`+
-      '<head><link rel="stylesheet" href="/style">'+
-      '</head><body>'+
-      '<h1>Hello</h1>'+
-      '<script src="/script"></script>'+
-      '</body></html>')
-    response.end()
-}else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
-    response.end()
-  }
+  console.log('方方说：得到 HTTP 路径 \n' + path)
+  response.write('Hi\n')
+  response.end()
 
   /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
 
