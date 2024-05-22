44a049f6c4fd10d01de3e45eff3d19777433bdda
xue <2723709118@qq.com>
2024-05-22 08:27:21 +0800

    修改server.js

--- node-demo/server.js
+++ node-demo/server.js
@@ -19,12 +19,29 @@ var server = http.createServer(function (request, response) {
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('方方说：得到 HTTP 路径 \n' + path)
-  response.write('Hi\n')
-  response.end()
-
+  if(path === '/'){  //如果用户请求的是 / 路径
+    var string = fs.readFileSync('./index.html', 'utf-8')
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path === './style.css'){
+    var string = fs.readFileSync('./style.css', 'utf-8')
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+  }else if(path === './main.js'){
+    var string = fs.readFileSync('./main.js', 'utf-8')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write(string)
+    response.end()
+  }else{
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('找不到对应的路径，你需要自行修改 index.js')
+    response.end()
+  }
   /******** 代码结束，下面不要看 ************/
-})
+
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
