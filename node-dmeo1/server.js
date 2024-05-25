0d14a27c84da576acd47f2da90500c086f1c35cf
xue <2723709118@qq.com>
2024-05-25 08:39:38 +0800

    5.25

--- node-dmeo1/server.js
+++ node-dmeo1/server.js
@@ -3,16 +3,16 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -20,22 +20,37 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
 
-  if(path === '/'){  //如果用户请求的是 / 路径
+  if (path === '/') {  //如果用户请求的是 / 路径
     var string = fs.readFileSync('./index.html', 'utf8')
+    var amount = fs.readFileSync('./db', 'utf8') //amount 是 string
+    string = string.replace('&&&amount&&&', amount)
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  } else if(path === '/style.css'){
+  } else if (path === '/style.css') {
     var string = fs.readFileSync('./style.css', 'utf8')
     response.setHeader('Content-Type', 'text/css')
     response.write(string)
     response.end()
-  } else if(path === '/main.js'){
+  } else if (path === '/main.js') {
     var string = fs.readFileSync('./main.js', 'utf8')
     response.setHeader('Content-Type', 'application/javascript')
     response.write(string)
     response.end()
-  }else {
+  } else if (path === '/pay') {
+    var amount = fs.readFileSync('./db', 'utf8')  //100
+    var newAmount = amount - 1
+    if (Math.random() > 0.5) {
+      fs.writeFileSync('./db', newAmount)
+      response.setHeader('Content-Type', 'image/jpg')
+      response.statusCode = 200
+      response.write(fs.readFileSync('./1.jpg!s2'))
+    } else {
+      response.statusCode = 400
+      response.write('fail')
+    }
+    response.end()
+  } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write('找不到对应的路径，你需要自行修改 server.js')
